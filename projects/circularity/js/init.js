e53130e925952bd90cd1fd088caf60130755f8a6
Camdenb2004 <camden.i.bayonne@gmail.com>
2021-07-06 15:14:27 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
+       function drawCircle(){
+        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+         physikz.addRandomVelocity(circle, canvas);
+        view.addChild(circle);
+        circles.push(circle)
+       };
+
+        // TODO 3 / 7 : Call the drawCircle() function 
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
         
-
-        // TODO 3 / 8 : Call the drawCircle() function 
-
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,12 +50,20 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+             
 
-            
-            // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            // TODO 5 : Call game.checkCirclePosition() on your circles.
+            game.checkCirclePosition( circles[0] );
+            game.checkCirclePosition( circles[1] );
+            game.checkCirclePosition( circles[2] );
+            game.checkCirclePosition( circles[3] );
+            game.checkCirclePosition( circles[4] );
 
-            // TODO 9 : Iterate over the array
            
             
         }
@@ -61,11 +80,11 @@ var init = function (window) {
                 circle.x = 0;
             }
             
-            // TODO 7 : YOUR CODE STARTS HERE //////////////////////
+            // TODO 6 : YOUR CODE STARTS HERE //////////////////////
             
 
 
-            // YOUR TODO 7 CODE ENDS HERE //////////////////////////
+            // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
         /////////////////////////////////////////////////////////////
